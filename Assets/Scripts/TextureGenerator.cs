471e4094b677429035ade7d4b22a2f4230b98e7b
John Cripe <johntc121@gmail.com>
2020-07-13 11:52:55 -0700

    land gen

---
+++ Assets/Scripts/TextureGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TextureGenerator
+{
+    public static Texture2D TextureFromColorMap(Color[] colorMap, int width, int height){
+        Texture2D texture = new Texture2D [width, height];
+        texture.SetPixels
+    }
+}
